82623fbccce6eda99dbf1a5c176458789b6919e7
Wojciech Adamek <36732892+wadamek65@users.noreply.github.com>
2024-06-12 00:07:39 +0200

    Remove unused import

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,6 @@ import sentry from '@sentry/astro'
 import icon from 'astro-icon'
 import sitemap from '@astrojs/sitemap'
 
-import robotsTxt from 'astro-robots-txt'
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wadamek.me',
