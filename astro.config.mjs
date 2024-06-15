dd4c63236873ecc220ad949fe21c01004cc733ce
Wojciech Adamek <36732892+wadamek65@users.noreply.github.com>
2024-06-15 17:02:04 +0200

    Reformat

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,17 +9,23 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wadamek.me',
-	integrations: [tailwind(), mdx(), sitemap(), icon({
-    iconDir: 'src/media/icons',
-  }), sentry({
-		dsn: 'https://0f5f72f1a2f880465f08659f4cea2694@o552412.ingest.us.sentry.io/4507390034509824',
-		sourceMapsUploadOptions: {
-			project: 'wadamek-me',
-			authToken: process.env.SENTRY_AUTH_TOKEN,
-		},
-		enabled: import.meta.env.PROD,
-		tracesSampleRate: 1.0,
-	})],
+	integrations: [
+		tailwind(),
+		mdx(),
+		sitemap(),
+		icon({
+			iconDir: 'src/media/icons',
+		}),
+		sentry({
+			dsn: 'https://0f5f72f1a2f880465f08659f4cea2694@o552412.ingest.us.sentry.io/4507390034509824',
+			sourceMapsUploadOptions: {
+				project: 'wadamek-me',
+				authToken: process.env.SENTRY_AUTH_TOKEN,
+			},
+			enabled: import.meta.env.PROD,
+			tracesSampleRate: 1.0,
+		}),
+	],
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 	},
